74d54e0b1d4262ffde7a96639db2e0d66428c42b
bbel <bweb33@protonmail.com>
2024-12-16 14:18:24 +0100

    fix 1.0: exo rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
 
+""" Module Rain """
+
 def find_next_wall(wall_index, walls):
-    """Trouve le prochain mur à droite d'un mur donné.
+    """Finds the next wall to the right of a given wall.
 
     Args:
-        wall_index (int): L'index du mur actuel.
-        walls (list): La liste des hauteurs des murs.
+        wall_index (int): The index of the current wall.
+        walls (list): The list of wall heights.
 
     Returns:
-        int: La hauteur du prochain mur ou 0 si aucun mur n'est trouvé.
+        int: The height of the next wall or 0 if no wall is found.
     """
     for wall in walls[wall_index + 1:]:
         if wall > 0:
             return wall
-    return 0  # Retourne 0 si aucun mur n'est trouvé
+    return 0  # Returns 0 if no wall is found
 
 
 def count_water_space(wall_index, walls):
-    """Compte l'espace d'eau entre le mur donné et le prochain mur.
+    """Counts the water space between the given wall and the next wall.
 
     Args:
-        wall_index (int): L'index du mur actuel.
-        walls (list): La liste des hauteurs des murs.
+        wall_index (int): The index of the current wall.
+        walls (list): The list of wall heights.
 
     Returns:
-        int: Le nombre d'espaces d'eau entre le mur actuel et le prochain mur.
+        int: The number of water spaces between the current wall and the next wall.
     """
     water_space = 0
     for wall in walls[wall_index + 1:]:
         if wall > 0:
             return water_space
         water_space += 1
-    return water_space  # Retourne l'espace d'eau compté
+    return water_space  # Returns the counted water space
 
 
 def rain(walls):
-    """Calcule la quantité d'eau de pluie retenue entre les murs.
+    """Calculates the amount of rainwater trapped between the walls.
 
     Args:
-        walls (list): La liste des hauteurs des murs.
+        walls (list): The list of wall heights.
 
     Returns:
-        int: La quantité totale d'eau de pluie retenue.
+        int: The total amount of trapped rainwater.
     """
     rainwater = 0
     for wall_index, wall_height in enumerate(walls):
