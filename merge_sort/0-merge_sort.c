eef9fb5cf18e702e4ca746c38bd7866bbbd1d4ed
bbel <bweb33@protonmail.com>
2025-02-09 18:53:04 +0100

    exo

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,61 +1,59 @@
 #include "sort.h"
 
 /**
- * unite - Combinates left and rigth arrays into the original one
- * @size:	[ int]	size of the array
- * @array:	[*int]	pointer to the head of the array
- * @left:	[*int]	pointer to left array
- * @right:	[*int]	pointer to right array
+ * unite - Combines left and right arrays into the original one
+ * @size:     [int] size of the array
+ * @array:    [*int] pointer to the head of the array
+ * @left:     [*int] pointer to left array
+ * @right:    [*int] pointer to right array
  **/
-
 void unite(size_t size, int *array, int *left, int *right)
 {
-	int i, j, k, l_lengh, r_lengh;
+    int i, j, k, l_length, r_length;
 
-	i = j = k = 0;
-	l_lengh = size / 2;
-	r_lengh = size - l_lengh;
+    i = j = k = 0;
+    l_length = size / 2;
+    r_length = size - l_length;
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(left, l_lengh);
-	printf("[right]: ");
-	print_array(right, r_lengh);
+    printf("Merging...\n");
+    printf("[left]: ");
+    print_array(left, l_length);
+    printf("[right]: ");
+    print_array(right, r_length);
 
-	while (i < l_lengh && j < r_lengh)
-		array[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
+    while (i < l_length && j < r_length)
+        array[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
 
-	while (i < l_lengh)
-		array[k++] = left[i++];
+    while (i < l_length)
+        array[k++] = left[i++];
 
-	while (j < r_lengh)
-		array[k++] = right[j++];
+    while (j < r_length)
+        array[k++] = right[j++];
 
-	printf("[Done]: ");
-	print_array(array, size);
+    printf("[Done]: ");
+    print_array(array, size);
 }
 
 /**
  * merge_sort - sorts in ascending order using Merge Sort method.
- * @array:	[*int]	pointer to the head of the array
- * @size:	[ int]	size of the array
+ * @array:    [*int] pointer to the head of the array
+ * @size:     [int] size of the array
  **/
-
 void merge_sort(int *array, size_t size)
 {
-	size_t middle, i;
-	int left[MAX], right[MAX];
+    size_t middle, i;
+    int left[MAX], right[MAX];
 
-	if (!array || size < 2)
-		return;
+    if (!array || size < 2)
+        return;
 
-	for (i = 0, middle = size / 2; i < middle; i++)
-		left[i] = array[i];
+    for (i = 0, middle = size / 2; i < middle; i++)
+        left[i] = array[i];
 
-	for (i = middle; i < size; i++)
-		right[i - middle] = array[i];
+    for (i = middle; i < size; i++)
+        right[i - middle] = array[i];
 
-	merge_sort(left, middle);
-	merge_sort(right, size - middle);
-	unite(size, array, left, right);
+    merge_sort(left, middle);
+    merge_sort(right, size - middle);
+    unite(size, array, left, right);
 }
--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -6,7 +6,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * merge_sort - sorts an array in ascending order using Merge Sort.
+ * @array:    [*int] pointer to the head of the array
+ * @size:     [size_t] size of the array
+ **/
 void merge_sort(int *array, size_t size);
+
+/**
+ * print_array - prints an array of integers.
+ * @array:    [*const int] pointer to the head of the array
+ * @size:     [size_t] size of the array
+ **/
 void print_array(const int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
